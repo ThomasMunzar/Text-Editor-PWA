2391c5a8a319aa08774c1ea673e81e0ab45d75b8
Thomas Munzar <tmunzar92@gmail.com>
2023-08-29 16:31:45 -0700

    got it to work '

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const { InjectManifest, GenerateSW } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -29,8 +29,8 @@ module.exports = () => {
         swSrc: './src-sw.js',
         swDest: 'service-worker.js',
       }),
-      //creates downloadable PWA
-      new GenerateSW(),
+      // creates downloadable PWA
+      // new GenerateSW(),
       new WebpackPwaManifest({
         name: 'Text-Editor 8000',
         short_name: 'T.E-8000',
